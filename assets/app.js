dcc12e633bf206ed2dcebcced4782ae09bd80ea6
Nadia-Kadri <nadiagrace96@gmail.com>
2019-11-01 17:40:41 -0400

    future and current first train time

--- assets/app.js
+++ assets/app.js
@@ -37,37 +37,68 @@ database.ref().on("child_added", function(snapshot) {
     let firstTime = snapshot.val().time;
     let frequency = snapshot.val().frequency;
 
-    console.log(`name: ${trainName}`)
-    console.log(`destination: ${destination}`)
-    console.log(`train start: ${firstTime}`)
-    console.log(`comes every: ${frequency} minutes`)
-
     let firstTimeConverted = moment(firstTime, "HH:mm").format("hh:mm a")
-    console.log(`train start momentjs: ${firstTimeConverted}`);
+    let timeDiff = moment().diff(moment(firstTime, "HH:mm"), "minutes")
+
+    console.log(`Name: ${trainName}`)
+    console.log(`Destination: ${destination}`)
+    console.log(`Train start: ${firstTimeConverted}`)
+    console.log(`Comes every: ${frequency} minutes`)
 
     let timeNow = moment().format("hh:mm a")
-    console.log(`time now momentjs: ${timeNow}`)
+    console.log(`Time now: ${timeNow}`)
+    
+    if(timeDiff > 0) {
+        let timeRemainder = timeDiff % frequency;
+        let minutesAway = frequency - timeRemainder;
 
-    let timeDiff = moment().diff(moment(firstTime, "HH:mm"), "minutes")
-    console.log(`difference between train start and now: ${timeDiff} minutes`)
+        let nextTrain = moment().add(minutesAway, "minutes");
+        let nextTrainConverted = moment(nextTrain).format("hh:mm a")
+        console.log(`Next train: ${nextTrainConverted}`)
+        console.log("--------------------------")
 
-    let timeRemainder = timeDiff % frequency;
-    console.log(timeRemainder);
-    let minutesAway = frequency - timeRemainder;
-    console.log(minutesAway);
+        var newRow = $("<tr>").append(
+            $("<td>").text(trainName),
+            $("<td>").text(destination),
+            $("<td>").text(firstTimeConverted),
+            $("<td>").text(frequency),
+            $("<td>").text(nextTrainConverted),
+            $("<td>").text(minutesAway)
+          );
+    
+        $("#train-schedule").append(newRow);
+    } else if (timeDiff < 0) {
+        let nextTrainConverted = firstTimeConverted
+        console.log(`Next train: ${nextTrainConverted}`)
+        console.log("--------------------------")
 
-    let nextTrain = moment().add(minutesAway, "minutes");
-    let nextTrainConverted = moment(nextTrain).format("hh:mm a")
-    console.log(nextTrainConverted)
+        let minutesAway = moment(nextTrainConverted, "hh:mm a").diff(moment(), "minutes") + 1
 
-    var newRow = $("<tr>").append(
-        $("<td>").text(trainName),
-        $("<td>").text(destination),
-        $("<td>").text(frequency),
-        $("<td>").text(nextTrainConverted),
-        $("<td>").text(minutesAway)
-      );
+        var newRow = $("<tr>").append(
+            $("<td>").text(trainName),
+            $("<td>").text(destination),
+            $("<td>").text(firstTimeConverted),
+            $("<td>").text(frequency),
+            $("<td>").text(nextTrainConverted),
+            $("<td>").text(minutesAway)
+          );
+    
+        $("#train-schedule").append(newRow);
+    } else {
+        console.log("Train is in station")
+        console.log("--------------------------")
 
-    $("#train-schedule").append(newRow);
-
+        var newRow = $("<tr>").append(
+            $("<td>").text(trainName),
+            $("<td>").text(destination),
+            $("<td>").text(firstTimeConverted),
+            $("<td>").text(frequency),
+            $("<td>").text("BOARDING NOW"),
+            $("<td>").text("BOARDING NOW")
+          );
+    
+        $("#train-schedule").append(newRow);
+    }
 });
+
+
