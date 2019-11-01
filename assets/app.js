3d57a7ad23b9a141dd92f360eab18c7cbbc7cc00
Nadia-Kadri <nadiagrace96@gmail.com>
2019-11-01 12:51:42 -0400

    next arrival and minutes away

--- assets/app.js
+++ assets/app.js
@@ -13,13 +13,13 @@ $("#run-search").on("click", function (event) {
 
     let trainName = $("#train-name-search").val().trim();
     let destination = $("#destination-search").val().trim();
-    let firstTrainTime = $("#first-time-search").val().trim();
+    let firstTime = $("#first-time-search").val().trim();
     let frequency = $("#frequency-search").val().trim();
 
     database.ref().push({
         name: trainName,
         destination: destination,
-        firstTime: firstTrainTime,
+        time: firstTime,
         frequency: frequency
     });
 
@@ -31,25 +31,43 @@ $("#run-search").on("click", function (event) {
 });
 
 database.ref().on("child_added", function(snapshot) {
-    console.log(snapshot.val());
 
     let trainName = snapshot.val().name;
     let destination = snapshot.val().destination;
-    let firstTrainTime = snapshot.val().firstTime;
+    let firstTime = snapshot.val().time;
     let frequency = snapshot.val().frequency;
 
-    console.log(trainName)
-    console.log(destination)
-    console.log(firstTrainTime)
-    console.log(frequency)
+    console.log(`name: ${trainName}`)
+    console.log(`destination: ${destination}`)
+    console.log(`train start: ${firstTime}`)
+    console.log(`comes every: ${frequency} minutes`)
+
+    let firstTimeConverted = moment(firstTime, "HH:mm").format("hh:mm a")
+    console.log(`train start momentjs: ${firstTimeConverted}`);
+
+    let timeNow = moment().format("hh:mm a")
+    console.log(`time now momentjs: ${timeNow}`)
+
+    let timeDiff = moment().diff(moment(firstTime, "HH:mm"), "minutes")
+    console.log(`difference between train start and now: ${timeDiff} minutes`)
+
+    let timeRemainder = timeDiff % frequency;
+    console.log(timeRemainder);
+    let minutesAway = frequency - timeRemainder;
+    console.log(minutesAway);
+
+    let nextTrain = moment().add(minutesAway, "minutes");
+    let nextTrainConverted = moment(nextTrain).format("hh:mm a")
+    console.log(nextTrainConverted)
 
     var newRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(destination),
         $("<td>").text(frequency),
-        $("<td>").text(""),
-        $("<td>").text("")
+        $("<td>").text(nextTrainConverted),
+        $("<td>").text(minutesAway)
       );
 
     $("#train-schedule").append(newRow);
+
 });
